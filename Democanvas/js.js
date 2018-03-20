f7072a68c538cf756d5eaa83655fc01c7ac63c93
igorWEBdeveloper <igorkh.developer@gmail.com>
2018-03-20 19:51:51 +0300

    add project and json

--- Democanvas/js.js
+++
@@ -1,8 +0,0 @@
-function myMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(55.755826, 37.6172999),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    }
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
